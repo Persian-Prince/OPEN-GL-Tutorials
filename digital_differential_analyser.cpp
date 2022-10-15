a51331138d80431f5ec17c48efc91978b5818dc9
Persian-Prince <92361948+Persian-Prince@users.noreply.github.com>
2022-10-15 13:33:00 +0530

    Create digital_differential_analyser.cpp

---
+++ digital_differential_analyser.cpp
@@ -0,0 +1,10 @@
+//Line generation using digital differential analyser DDA algorithm.
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    
+}
