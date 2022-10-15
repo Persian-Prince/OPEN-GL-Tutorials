17139c56686ba58deba21f88c0f52ad6f061d2a6
Persian-Prince <92361948+Persian-Prince@users.noreply.github.com>
2022-10-15 13:43:12 +0530

    Create parametric_circle_algorithm.cpp

---
+++ parametric_circle_algorithm.cpp
@@ -0,0 +1,10 @@
+// Program to draw a circle using parametric algorithm.
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
