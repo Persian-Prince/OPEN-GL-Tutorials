ba75726d00acaa6680c27f3fa65da15eaff43957
Persian-Prince <92361948+Persian-Prince@users.noreply.github.com>
2022-10-15 13:43:47 +0530

    Create polynomial_circle.cpp

---
+++ polynomial_circle.cpp
@@ -0,0 +1,10 @@
+// Program to draw a circle using polynomial algorithm.
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
