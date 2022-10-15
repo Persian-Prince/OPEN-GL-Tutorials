843875055dfb0073cb5c9072acf7b5eac51bce62
Persian-Prince <92361948+Persian-Prince@users.noreply.github.com>
2022-10-15 13:34:50 +0530

    Create bresenham_line_generation.cpp

---
+++ bresenham_line_generation.cpp
@@ -0,0 +1,10 @@
+// Line generation using Bresenham's Line Generation Algorithm.
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
